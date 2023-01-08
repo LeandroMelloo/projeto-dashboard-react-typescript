7df8495c7f1fb9c014374df4ae7eeeaa5ae5b420
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-01-07 22:08:43 -0300

    Criando o header principal

--- minha-carteira/src/components/MainHeader/index.tsx
+++ minha-carteira/src/components/MainHeader/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+// useMemo => é um Hooks do React que decora o valor.
+// useCallback => é um Hooks do React que memoriza a função muda
+import React, {useMemo} from 'react';
 
-import { Container, } from './styles';
+import emojis from '../../utils/emojis';
+
+import { 
+    Container, 
+    Profile, 
+    UserName, 
+    Welcome 
+} from './styles';
 
 const MainHeader: React.FC = () => {
-  return (
-    <Container>
-      <h1>MainHeader</h1>
-    </Container>
-  );
+
+    const emoji = useMemo(() => {
+        // Math.floor => arredonda os valores com uma casa decimal 8.9999 fica 9
+        // Math.random() * 10 => gera valores aleatorios de 0 a 10
+        const indice = Math.floor(Math.random() * emojis.length); 
+        console.log(indice);
+        return emojis[indice];
+    }, []);
+
+    return (
+        <Container>
+            <h1>Toogle</h1>
+
+            <Profile>
+                <Welcome>Olá {emoji}</Welcome>
+                <UserName>Leandro Mello</UserName>
+            </Profile>
+        </Container>
+    );
 }
 
 export default MainHeader;
