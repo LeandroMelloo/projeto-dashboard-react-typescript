413d089a1dfaf63f97e05e1cff2063f4e5180c6c
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-01-08 14:34:17 -0300

    Criando o componente Toggle

--- minha-carteira/src/components/MainHeader/index.tsx
+++ minha-carteira/src/components/MainHeader/index.tsx
@@ -2,6 +2,7 @@
 // useCallback => é um Hooks do React que memoriza a função muda
 import React, {useMemo} from 'react';
 
+import Toggle from '../../components/Toogle';
 import emojis from '../../utils/emojis';
 
 import { 
@@ -17,13 +18,12 @@ const MainHeader: React.FC = () => {
         // Math.floor => arredonda os valores com uma casa decimal 8.9999 fica 9
         // Math.random() * 10 => gera valores aleatorios de 0 a 10
         const indice = Math.floor(Math.random() * emojis.length); 
-        console.log(indice);
         return emojis[indice];
     }, []);
 
     return (
         <Container>
-            <h1>Toogle</h1>
+            <Toggle />
 
             <Profile>
                 <Welcome>Olá {emoji}</Welcome>
